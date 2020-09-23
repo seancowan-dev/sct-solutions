65713456de4855c11c8256957b4bcf39ccbef8a0
Sean <sean.cowan1@gmail.com>
2020-09-23 18:18:52 -0400

    implemented basic CKEditor HOC

--- src/views/Primary/Primary.js
+++ src/views/Primary/Primary.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import Header from '../../models/Header.model';
 import Login from '../../models/Login.model';
+import CKEditor from '../../comps/ckeditor';
 
 const Primary = observer((props) => {
 
@@ -9,6 +10,7 @@ const Primary = observer((props) => {
         <div className="main-container">
             <Login />
             <Header />
+            <CKEditor />
         </div>
     );    
 });
